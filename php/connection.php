b13564101cc7b9f5faa01d059c75e286a2d98b30
fafaraai <60306193+fafaraai@users.noreply.github.com>
2021-10-24 19:25:36 +0800

    Update connection.php

--- php/connection.php
+++ php/connection.php
@@ -1,13 +1,4 @@
 <?php
-
-$dbhost = "localhost";
-$dbuser = "root";
-$dbpass = "";
-$dbname = "online_quizdb";
-
-$con = mysqli_connect($dbhost,$dbuser,$dbpass,$dbname);
-if(!$con)
-{
-
-	die("failed to connect!");
-}
+$link=mysqli_connect("localhost","root","");
+mysqli_select_db($link,"online_quizdb");
+?>
