d911d4727ae976337d6dd36a08671760cda8ecaf
ariamazile <91361944+ariamazile@users.noreply.github.com>
2021-10-26 22:02:01 +0800

    ...

--- admin/login.php
+++ admin/login.php
@@ -8,47 +8,44 @@ include "../conn/connection.php";
 <head>
     <meta charset="utf-8">
     <meta http-equiv="x-ua-compatible" content="ie=edge">
-    <title>Admin Login</title>
+    <title>Admin LogIn</title>
     <meta name="description" content="">
     <meta name="viewport" content="width=device-width, initial-scale=1">
 
-    <link rel="stylesheet" href="../css/bootstrap.min.css">
     <link rel="stylesheet" href="../css/style.css">
 </head>
 
 <body>
-<div style = "margin: ;
-    padding: 0;
+<div style = "margin: 0;
+    padding: 10;
     text-align: center;
-    height: 5% ;
+    height: 10% ;
     width: 50% center;
-    background-repeat: no-repeat;"> <img src = "../image/um2.jpg" height="300" width="800" > </div>
+    background-repeat: no-repeat;"> <img src = "../image/um2.jpg" height="700" width="1000" > </div>
     
 	<div class="error-pagewrap">
 		<div class="error-page-int">
 			<div class="text-center m-b-md custom-login">
 			</div>
+            <div class= "center">
+            <h1>Admin</h1>
 			<div class="content-error">
 				<div class="hpanel">
                     <div class="panel-body">
-					<h3>ADMIN LOGIN FORM</h3>
                         <form action="#" name="form1" method="post">
-                            <div class="form-group">
-                                <label class="control-label" for="username">Username</label>
-                                <input type="text" placeholder="username" title="Please enter you username" required="" value="" name="username" id="username" class="form-control">
-
+                            <div class="txt_field">
+                            <input type="text" title="Please enter you username" required="" value="" name="username" id="username" class="form-control">
+                            <span></span>
+                            <label class="userlabel" for="username">Username:</label>
+                             </div>
+                                <div class="txt_field">
+                                <input type="password" title="Please enter your password"  required="" value="" name="password" id="password" class="form-control">
+                                <span></span>
+                                <label class="passlabel" for="password">Password:</label>
                             </div>
-                            <div class="form-group">
-                                <label class="control-label" for="password">Password</label>
-                                <input type="password" title="Please enter your password" placeholder="******" required="" value="" name="password" id="password" class="form-control">
-
-                            </div>
-
-                            <button type="submit" name="login" class="btn btn-success btn-block loginbtn">Login</button>
-                           
-
+                            <button type="submit" name="login" class="btn btn-success btn-block loginbtn">Sign In</button>
                             <div class="alert alert-danger" id="failure" style="margin-top: 10px; display: none">
-                                <strong>Does Not Match!</strong> Invalid Username or Password.
+                                <strong>Does Not Match!</strong>  <p>Invalid Username or Password.</p>
                             </div>
                         </form>
                     </div>
@@ -57,8 +54,9 @@ include "../conn/connection.php";
 
 		</div>   
     </div>
+    </div>
 
-<?php
+<?php 
 if(isset($_POST["login"]))
 {
     $count=0;
