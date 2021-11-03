cb257bb426dffcf450e1a0e960b847ce9c98a9f1
rairai <60306193+fafaraai@users.noreply.github.com>
2021-11-03 17:19:41 +0800

    Update createquiz.php

--- admin/createquiz.php
+++ admin/createquiz.php
@@ -1,5 +1,5 @@
 <?php
-include "header.php";
+include "../admin/header.php";
 include "../conn/connection.php";
 ?>
 
@@ -11,7 +11,7 @@ include "../conn/connection.php";
                             <form name="form1" action="" method="post">
                             <div class="card-header"><strong>Add Exam</strong>
                                 <div class="form-group"><label for="exam_name" class=" form-control-label">New Exam </label><input type="text" name="exam_name" placeholder="Add Exam Categories" class="form-control"></div>
-                                    <div class="form-group"><label for="exam_time_in_minutes" class=" form-control-label">Exam Time</label><input type="text"  placeholder="Exam Time In Minutes" class="form-control" name="exam_time_in_minutes"></div>
+                                    <div class="form-group"><label for="quiz_time_in_minutes" class=" form-control-label">Exam Time</label><input type="text"  placeholder="Quiz Time In Minutes" class="form-control" name="quiz_time_in_minutes"></div>
                                     <div class="form-group">
                                         <input type="submit" name="submit1" value="Add Exam" class="btn-btn-success">
                                     </div>                           
@@ -21,11 +21,12 @@ include "../conn/connection.php";
 <?php
     if(isset($_POST["submit1"]))
     {
-        mysqli_query($link,"insert into quiz values(NULL,'$_POST[exam_name]','$_POST[exam_time_in_minutes]')") or die(mysqli_error($link));
+        mysqli_query($link,"insert into quiz values(NULL,'$_POST[exam_name]','$_POST[quiz_time_in_minutes]')") or die(mysqli_error($link));
 
         ?>
         <script type="text/javascript">
             alert("exam added successfully");
+            window.location.href=window.location.href;
         </script>
         <?php
     }
