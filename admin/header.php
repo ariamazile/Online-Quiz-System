3664558e82f02992ee78d14b83c178f019b8e86c
rairai <60306193+fafaraai@users.noreply.github.com>
2021-11-16 23:21:24 +0800

    Update header.php

--- admin/header.php
+++ admin/header.php
@@ -31,7 +31,6 @@
             </center>
             <a href="../admin/header.php"><i class="fas fa-chalkboard"></i><span>Dashboard</span></a>
             <a href="../admin/createquiz.php"><i class="fas fa-plus-square"></i><span>Create Quiz</span></a>
-            <a href="../admin/editquiz.php"><i class="fas fa-edit"></i><span>Add & Edit Quiz</span></a>
             <a href="../admin/selectquestionnaire.php"><i class="fas fa-plus-square"></i><span>Manage Questionnaire</span></a>
             <a href="../admin/postedquiz.php"><i class="fas fa-upload"></i><span>Posted Quiz</span></a>
             <a href="../admin/completedquiz.php"><i class="fas fa-tasks"></i><span>Completed Quiz</span></a>
