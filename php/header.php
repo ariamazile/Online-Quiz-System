80b09d0a22970fccdda4ae3078b25b0fd9fac461
rairai <60306193+fafaraai@users.noreply.github.com>
2021-11-26 11:13:50 +0800

    Update header.php

--- php/header.php
+++ php/header.php
@@ -1,9 +1,17 @@
+<?php
+include "conn/connection.php";
+session_start();
+if(!isset($_SESSION["username"]))
+
+
+?>
+
 <!DOCTYPE html>
 <html lang="en" dir="ltr" >
     <head>   
 <meta charset="utf-8">
 <title>Dashboard</title>
-<link rel="stylesheet" href="../css/dashboard.css">
+<link rel="stylesheet" href="css/dashboard.css">
 <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.13.0/css/all.min.css">
 </head>
 <body>
@@ -17,19 +25,47 @@
             <h3></h3>
         </div>
         <div class="right_area">
-            <a href="login.php" class="logout_btn">Log out</a>
+            <a href="logout.php" class="logout_btn">Log out</a>
         </div>
     </header>
     
         <div class="sidebar">
             <center>
-                <img src="../image/2.jpg" class="profile_image" alt="">
-                <h4>Student</h4>
+                <img src="image/2.jpg" class="profile_image" alt="">
+                <h4><?php echo $_SESSION["username"]; ?></h4>
             </center>
-            <a href="#"><i class="fas fa-history"></i></i><span>Quiz History</span></a>
+            <a href="selectexam.php"><i class="fas fa-history"></i></i><span>Select Exam</span></a>
+            <a href="quiz_result.php"><i class="fas fa-history"></i></i><span>Quiz History</span></a>
+
+            <div class="col-lg-12 col-md-6 col-sm-6 col-xs-12 text-right">
+            <ul class="breadtime-menu"
+            <li><div id="countdowntimer" style="display: block;"></div>
+            </li>
            
         </div>
   
+
+<script type="text/javascript">
+    setInterval(function(){
+        timer();
+    },1000);
+    function timer()
+    {
+    var xmlhttp=new XMLHttpRequest();
+    xmlhttp.onreadystatechange=function() {
+        if(xmlhttp.readyState == 4 && xmlhttp.status==200){
+           
+           if(xmlhttp.responseText=="00:00:01")
+           {
+               window.location="result.php";
+           }
+           document.getElementById("countdowntimer"). innerHTML=xmlhttp.responseText;
+        }
+    };
+    xmlhttp.open("GET", "forajax/load_timer.php", true);
+    xmlhttp.send(null);
+    }
+</script>
 
 </body>
 </html>
