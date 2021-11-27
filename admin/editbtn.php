5583b8db44024a74d4de1b94e6d5117397840030
ariamazile <91361944+ariamazile@users.noreply.github.com>
2021-11-27 19:39:12 +0800

    .....

--- admin/editbtn.php
+++ admin/editbtn.php
@@ -1,5 +1,6 @@
 <?php
 include "../conn/connection.php";
+include "../admin/header.php";
 $quiz_id=$_GET["id"];
 $res=mysqli_query($link, "SELECT * from quiz where quiz_id=$quiz_id");
 while($row=mysqli_fetch_array($res))
@@ -8,11 +9,11 @@ while($row=mysqli_fetch_array($res))
     $quiz_time_in_minutes=$row["quiz_time_in_minutes"];
 }
 ?>
-
+  <link rel="stylesheet" href="../css/editbtn.css">
 <div class="page-title">
                         <h1>Edit Exam</h1>
                     </div>
-    
+    <div class="editbtn">
                             <form name="form1" action="" method="post">
                             <div class="card-header"><strong>Edit Exam</strong>
                                 <div class="form-group"><label for="exam_name" class=" form-control-label">New Exam </label><input type="text" name="exam_name" placeholder="Add Exam Categories" class="form-control" value="<?php echo $exam_name; ?>"></div>
@@ -22,7 +23,7 @@ while($row=mysqli_fetch_array($res))
                                     </div>                           
 
                             </div>
-
+                            </div>
 <?php
     if(isset($_POST["submit1"]))
     {
